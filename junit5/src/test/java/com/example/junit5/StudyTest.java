1c9176ac613042b1d43420200b148f6eff52bb72
wehwan <we_hwan@naver.com>
2022-02-13 19:51:48 +0900

    test: junit5 기본 사용법 테스트 추가

--- junit5/src/test/java/com/example/junit5/StudyTest.java
+++ junit5/src/test/java/com/example/junit5/StudyTest.java
@@ -1,14 +1,43 @@
 package com.example.junit5;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
     void create() {
         Study study = new Study();
         assertNotNull(study);
+    }
+
+    @Disabled
+    @Test
+    void disable() {
+        Study study = new Study();
+        assertNotNull(study);
+    }
+
+    @BeforeAll
+    static void before_all() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    @DisplayName("모든 테스트 종료 후")
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("after each");
     }
 }
